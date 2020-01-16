f7e0d74cdf99e7e6d3a24f57617b98e524e33a0d
tahamuneeb <tahamuneeb2@gmail.com>
2020-01-16 16:33:30 +0500

    Added some test cases

--- FastMoviesTests/FastMoviesTests.swift
+++ FastMoviesTests/FastMoviesTests.swift
@@ -7,9 +7,39 @@
 //
 
 import XCTest
+import CoreData
 @testable import FastMovies
 
 class FastMoviesTests: XCTestCase {
+    
+    func testYear(){
+        let refDate = "2019-1-12"
+        let outputYear = Helper.year(string: refDate)
+        XCTAssertEqual("2019", outputYear)
+    }
+    
+    func testFilterDate(){
+        let date = Date()
+        XCTAssertEqual(date.movieFilterDate(), "2020-01-01")
+    }
+    
+    func testDeleteFunc(){
+        let interactor = MovieListInteractor()
+        interactor.clearStorage(type: .popular)
+        let movies = interactor.fetchFromStorage(type: .popular)
+        XCTAssertEqual(movies, [])
+    }
+    
+    func testsetMovieSave(){
+        
+        let movie = Movie.init(context: SingleTon.shared.cdContext)
+        movie.setType(type: .popular)
+        
+        let interactor = MovieListInteractor()
+        interactor.clearStorage(type: .popular)
+        interactor.saveToStorage(type: .popular)
+        XCTAssertEqual(interactor.fetchFromStorage(type: .popular).count, 1)
+    }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
